006067fd26dbf96c0a62d260271f6cc329037e1f
jarble <jarble@gmail.com>
2018-11-10 04:10:39 -0500

    Added another interpreter

---
+++ pack.pl
@@ -0,0 +1,12 @@
+name('imperative_prolog').
+title('An interpreter for imperative algorithms in Prolog').
+
+version('0.1').
+download('https://github.com/jarble/transpiler/archive/v0.1.zip').
+
+
+author('Anderson Green','jarble@gmail.com').
+packager('Anderson Green','jarble@gmail.com').
+maintainer('Anderson Green','jarble@gmail.com').
+
+home('https://github.com/jarble/imperative_prolog/').
--- prolog/imperative_programming_example.pl
+++ prolog/imperative_programming_example.pl
@@ -3,11 +3,20 @@
 
 main :-
 	imperative(Result,[
+		r = 1,
+		the_list = [3,4,5],
+		sort(the_list),
+		z = a_function(2+r,3),
+		a1 = member(3,[3,4]),
+		a2 = (true;true),
 		a = 1,
 		d = sin(0.1)**2 + cos(0.1)**2,
+		f = \+(a = d),
 		b = 2,
 		z = b + 1,
-		while(a < 10,[
+		while(a =< 10,[
+			appended_list = [1+1,2,3]+[1,2,3],
+			writeln(appended_list),
 			bool_var = (3>4),
 			the_list = sorted([3,5,32,1,98,8,7]),
 			list2 = [4,2,6],
@@ -23,25 +32,25 @@ main :-
 				[return = 5]
 		])
 	]),
-	writeln(Result).
+	writeln(Result),!.
 
 imperative_test(Input,Output) :-
 	Input = [x:1],
 	imperative(Input,Output,[
 		'x' = 'x' + 1
-	]).
+	]),!.
 
 while(List,Result,Condition,Statements) :-
 	(member(return:_,List),List = Result);
 	get_var(List,Condition_,Condition),
 	(Condition_ -> (imperative(List,Result_,Statements),while(Result_,Result,Condition,Statements));
-	List=Result).
+	List=Result),!.
 
 %in-place sort
 imperative_(List,Result,sort(A)) :-
 	imperative(List,Result,[
 		A = sorted(A)
-	]).
+	]),!.
 
 imperative_(List,Result,Command) :-
 	(member(A:_,List),
@@ -49,100 +58,171 @@ imperative_(List,Result,Command) :-
 	(Command = increment(A,B), A2 is A1 + B;
 	Command = increment(A), A2 is A1 + 1;
 	Command = decrement(A), A2 is A1 - 1;
-	Command = (A = B), get_var(List,A2,B)).
+	Command = (A = B), get_var(List,A2,B)),!.
 
 imperative_(List,Result,while(Condition,Statements)) :-
-	while(List,Result,Condition,Statements).
+	while(List,Result,Condition,Statements),!.
 
 imperative_(List,List,writeln(A)) :-
-	get_var(List,A_,A),
-	writeln(A_).
+	get_var(List,A_,A),!,
+	writeln(A_),!.
 
 imperative_(List,Result,(Condition -> Statements)) :-
 	get_var(List,Condition_,Condition),
-	(Condition_ -> imperative(List,Result,Statements);List=Result).
+	(Condition_ -> imperative(List,Result,Statements);List=Result),!.
 
-imperative(Result,Steps) :- imperative([],Result,Steps).
+imperative(Result,Steps) :- imperative([],Result1,Steps),memberchk(return:Result,Result1),!.
 
 imperative(List,Result,[]) :-
-	List = Result.
+	List = Result,!.
 	
 imperative(List,Result,[Step|Rest]) :-
 	(member(return:_,List),List = Result);
-	imperative_(List,List1,Step), imperative(List1,Result,Rest).
+	imperative_(List,List1,Step),!,imperative(List1,Result,Rest),!.
 
-get_var(List,A2,B) :- phrase(get_var(List,B),[A2]).
+get_var(List,A2,B) :- phrase(get_var(List,B),[A2]),!.
 
 get_var(List,get_var(A)) -->
-	{get_var(List,A_,A)},[A_].
+	{get_var(List,A_,A)},[A_],!.
 
 get_var(List,str(A)) -->
-	{get_var(List,A_,A)},[A_].
+	{get_var(List,A_,A)},[A_],!.
 
 get_var(List,sorted(A)) -->
-	{get_var(List,A_,A),sort(A_,A1)},[A1].
+	{get_var(List,A_,A),sort(A_,A1)},[A1],!.
 
 get_var(List,B) -->
-	{member(B:A2,List)},[A2].
+	{member(B:A2,List)},[A2],!.
 
 get_var(_,B) -->
-	{number(B)},[B].
+	{number(B)},[B],!.
 
+get_var(_,[]) -->
+	[[]],!.
+get_var(List,[A]) -->
+	{get_var(List,A_,A)},[[A_]],!.
+get_var(List,[A|B]) -->
+	{get_var(List,A_,A),get_var(List,B_,B)},[[A_|B_]],!.
 get_var(_,B) -->
-	{is_list(B)},[B].
+	{is_list(B)},[B],!.
+
+get_var(List,log(A)) -->
+	{get_var(List,A_,A), number(A_), A2 is log(A_)},[A2],!.
 
 get_var(List,sin(A)) -->
-	{get_var(List,A_,A), number(A_), A2 is sin(A_)},[A2].
+	{get_var(List,A_,A), number(A_), A2 is sin(A_)},[A2],!.
 
 get_var(List,cos(A)) -->
-	{get_var(List,A_,A), number(A_), A2 is cos(A_)},[A2].
+	{get_var(List,A_,A), number(A_), A2 is cos(A_)},[A2],!.
 
 get_var(List,tan(A)) -->
-	{get_var(List,A_,A), number(A_), A2 is tan(A_)},[A2].
+	{get_var(List,A_,A), number(A_), A2 is tan(A_)},[A2],!.
+
+get_var(List,asin(A)) -->
+	{get_var(List,A_,A), number(A_), A2 is asin(A_)},[A2],!.
+
+get_var(List,acos(A)) -->
+	{get_var(List,A_,A), number(A_), A2 is acos(A_)},[A2],!.
+
+get_var(List,atan(A)) -->
+	{get_var(List,A_,A), number(A_), A2 is atan(A_)},[A2],!.
 
 get_var(List,(A+B)) -->
-	{get_var(List,A_,A), get_var(List,B_,B), number(A_),number(B_), A2 is A_ + B_},[A2].
+	{get_var(List,A_,A), get_var(List,B_,B), number(A_),number(B_), A2 is A_ + B_},[A2],!.
 
 get_var(List,(A+B)) -->
-	{get_var(List,A_,A), get_var(List,B_,B), is_list(A_),is_list(B_), append(A_,B_,A2)},[A2].
+	{get_var(List,A_,A), get_var(List,B_,B), is_list(A_),is_list(B_), append(A_,B_,A2)},[A2],!.
 
 get_var(List,(A-B)) -->
-	{get_var(List,A_,A), get_var(List,B_,B), A2 is A_ - B_},[A2].
+	{get_var(List,A_,A), get_var(List,B_,B), A2 is A_ - B_},[A2],!.
 	
 get_var(List,(A/B)) -->
-	{get_var(List,A_,A), get_var(List,B_,B), A2 is A_ / B_},[A2].
+	{get_var(List,A_,A), get_var(List,B_,B), A2 is A_ / B_},[A2],!.
+
+get_var(List,(A \== B)) -->
+	get_var(List,(A \= B)),!.
+
+get_var(List,(A \= B)) -->
+	{get_var(List,A_,A), get_var(List,B_,B),eval_bool((A_ \= B_),Result)},[Result],!.
 
 get_var(List,(A*B)) -->
-	{get_var(List,A_,A), get_var(List,B_,B), A2 is A_ * B_},[A2].
+	{get_var(List,A_,A), get_var(List,B_,B), A2 is A_ * B_},[A2],!.
 
 get_var(List,(A**B)) -->
-	{get_var(List,A_,A), get_var(List,B_,B), A2 is A_ ** B_},[A2].
+	{get_var(List,A_,A), get_var(List,B_,B), A2 is A_ ** B_},[A2],!.
 
 get_var(List,(A mod B)) -->
-	{get_var(List,A_,A), get_var(List,B_,B), A2 is A_ mod B_},[A2].
+	{get_var(List,A_,A), get_var(List,B_,B), A2 is A_ mod B_},[A2],!.
+
+get_var(List,(A >= B)) -->
+	{get_var(List,A_,A), get_var(List,B_,B),eval_bool((A_ >= B_),Result)},[Result],!.
+
+get_var(List,(A =< B)) -->
+	{get_var(List,A_,A), get_var(List,B_,B),eval_bool((A_ =< B_),Result)},[Result],!.
 
 get_var(List,(A>B)) -->
-	{get_var(List,A_,A), get_var(List,B_,B)},[A_ > B_].
+	{get_var(List,A_,A), get_var(List,B_,B),eval_bool((A_ > B_),Result)},[Result],!.
 
 get_var(List,(A<B)) -->
-	{get_var(List,A_,A), get_var(List,B_,B)},[A_ < B_].
+	{get_var(List,A_,A), get_var(List,B_,B),eval_bool((A_ < B_),Result)},[Result],!.
 
 get_var(List,(A,B)) -->
-	{get_var(List,A_,A), get_var(List,B_,B)},[(A_,B_)].
+	{get_var(List,A_,A), get_var(List,B_,B),eval_bool((A_,B_),Result)},[Result],!.
+
+get_var(List,(A==B)) -->
+	get_var(List,(A=B)),!.
 
 get_var(List,(A=B)) -->
-	{get_var(List,A_,A), get_var(List,B_,B)},[(A_=B_)].
+	{get_var(List,A_,A), get_var(List,B_,B),eval_bool((A_=B_),Result)},[Result],!.
+
+get_var(List,member(A,B)) -->
+	{get_var(List,A_,A), get_var(List,B_,B), eval_bool(member(A_,B_),Result)},[Result],!.
+
+get_var(List,imperative_factorial(Input,Output)) -->
+	{imperative(Output,[
+		i = 1,
+		result = 1,
+		while((i =< Input),[
+			result = result * i,
+			i = i + 1
+		]),
+		return = result
+	])},[Output].
+
+
+get_var(List,pow(A,B)) -->
+	get_var(List,(A**B)),!.
+
+get_var(List,(A^B)) -->
+	get_var(List,(A**B)),!.
+	
+get_var(List,\+(A)) -->
+	get_var(List,not(A)),!.
+
+get_var(List,not(A)) -->
+	{get_var(List,A_,A),eval_bool(not(A_),Result)},[Result],!.
 	
 get_var(List,(A;B)) -->
-	{get_var(List,A_,A), get_var(List,B_,B)},[(A_;B_)].
+	{get_var(List,A_,A), get_var(List,B_,B),eval_bool((A_; B_),Result)},[Result],!.
 
 get_var(_,true) -->
-	[true].
-	
+	[true],!.
 get_var(_,false) -->
-	[false].
+	[false],!.
+
+get_var(List,A) -->
+	{callable(A),A =.. [Name|Rest],dif(Rest,[]),get_var(List,Rest_,Rest),B =.. [Name|[Rest_,Output]],call(B)},[Output].
+
+a_function([A,B],Output) :-
+	imperative(Output,[
+		z = 1,
+		return = A+B+z
+	]).
+
+eval_bool(A,B) :-
+	A -> B = true;B=false,!.
 
 % code by @svick, modified to use dif/2 instead of (\=)/2
 replaceP(_, _, [], []).
-replaceP(O, R, [O|T], [R|T2]) :- replaceP(O, R, T, T2).
-replaceP(O, R, [H|T], [H|T2]) :- dif(H,O), replaceP(O, R, T, T2).
+replaceP(O, R, [O|T], [R|T2]) :- replaceP(O, R, T, T2),!.
+replaceP(O, R, [H|T], [H|T2]) :- dif(H,O), replaceP(O, R, T, T2),!.
