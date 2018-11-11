d129b4312ac3967aaede084e825c21e53693372b
jarble <jarble@gmail.com>
2018-11-11 18:10:00 -0500

    added conditional statements

--- prolog/imperative_interpreter.pl
+++ prolog/imperative_interpreter.pl
@@ -3,9 +3,22 @@
 
 main :-
 	imperative_statements((
-		A = increment_var(1)*2
+		A=1,
+		N=0,
+		while(A<50,(
+			A=A+2,
+			N=N-1
+		)),
+		((N<10)->(
+			N=N/3,
+			N=N+1
+		);
+		(N>10) ->(
+			N=N*3
+		))
 	)),
-	writeln(A).
+	writeln(A),
+	writeln(N).
 
 increment_var(A,B) :-
 	imperative_statements((
@@ -26,12 +39,6 @@ imperative_statements(A) :-
 	set_var(A,Names,Input,Output),
 	Names=Output.
 
-set_var(A,Names,Input) :-
-	length(Input,L),
-	length(Output,L),
-	set_var(A,Names,Input,Output),
-	Names=Output.
-
 set_var((A,B),Var_names,Input,Output) :-
 		set_var(A,Var_names,Input,Output1),
 		set_var(B,Var_names,Output1,Output).
@@ -47,9 +54,19 @@ set_var_(Name=Value1,[Name1|Name2],[Var1|Var2],[Output1|Output2]) :-
 	set_var_(Name=Value1,Name2,Var2,Output2).
 	
 set_var(while(A,B),Names,Vars,Result) :-
-    get_var(A,[Names,Vars],A1),
-    (((A1==true)->(set_var(B,Names,Vars,Result1),set_var(while(A,B),Names,Result1,Result)));
-    A1==false,Vars=Result).
+	get_var(A,[Names,Vars],A1),
+	(((A1==true)->(set_var(B,Names,Vars,Result1),set_var(while(A,B),Names,Result1,Result)));
+	A1==false,Vars=Result).
+
+set_var((A->B),Names,Vars,Result) :-
+	get_var(A,[Names,Vars],A1),
+	(((A1==true)->(set_var(B,Names,Vars,Result)));
+	A1==false,Vars=Result).
+
+set_var(((A->B);C),Names,Vars,Result) :-
+	get_var(A,[Names,Vars],A1),
+	(((A1==true)->(set_var(B,Names,Vars,Result)));
+	A1==false,set_var(C,Names,Vars,Result)).
 
 get_var(Name,[[Name1],[Var1]],Output) :-
 	var(Name),Name==Name1,
@@ -69,6 +86,10 @@ get_var(A+B,Vars,Output) :-
 	get_var(A,Vars,A1),
 	get_var(B,Vars,B1),
 	Output is A1+B1.
+get_var(A-B,Vars,Output) :-
+	get_var(A,Vars,A1),
+	get_var(B,Vars,B1),
+	Output is A1-B1.
 get_var(A*B,Vars,Output) :-
 	get_var(A,Vars,A1),
 	get_var(B,Vars,B1),
@@ -83,33 +104,33 @@ get_var(A**B,Vars,Output) :-
 	Output is A1**B1.
 
 get_var((A,B),Vars,Result) :-
-    get_var([A,B],Vars,[A1,B1]),
-    (A1,B1,Result=true;([A1,B1]=[true,false];[A1,B1]=[false,true]),Result=false).
+	get_var([A,B],Vars,[A1,B1]),
+	(A1,B1,Result=true;([A1,B1]=[true,false];[A1,B1]=[false,true]),Result=false).
 
 get_var((A;B),Vars,Result) :-
-    (get_var(A,Vars,A1),call(A1);
-    get_var(B,Vars,B1),call(B1)) -> (Result = true);
-    (get_var(A,Vars,A1),A1=false,Result=false).
+	(get_var(A,Vars,A1),call(A1);
+	get_var(B,Vars,B1),call(B1)) -> (Result = true);
+	(get_var(A,Vars,A1),A1=false,Result=false).
 
 get_var(A<B,Vars,Result) :-
 	get_var(B>A,Vars,Result).
 get_var(A>B,Vars,Result) :-
-    %comparison of variables
-    get_var([A,B],Vars,[A1,B1]),
-    (A1>B1,Result=true;A1<B1,Result=false).
-    
+	%comparison of variables
+	get_var([A,B],Vars,[A1,B1]),
+	(A1>B1,Result=true;A1<B1,Result=false).
+	
 get_var(A==B,Vars,Result) :-
-    %comparison of variables
-    get_var([A,B],Vars,[A1,B1]),
-    (A1==B1,Result=true;A1\=B1,Result=false).
+	%comparison of variables
+	get_var([A,B],Vars,[A1,B1]),
+	(A1==B1,Result=true;A1\=B1,Result=false).
 
 get_var(Input,Vars,Output1) :-
-    (\+number(Input)),
-    Input =.. [Name|Params],
-    \+member(Name,['=',==,'->',not,'[|]',',',';',+,-,*,/,**,^,writeln]),
-    length(Params,Params_length),
-    Params_length > 0,
-    get_var(Params,Vars,Params1),
-    append([Name|Params1],[Output1],Input0),
-    Input1 =.. Input0,
-    call(Input1).
+	(\+number(Input)),
+	Input =.. [Name|Params],
+	\+member(Name,['=',==,'->',not,'[|]',',',';',+,-,*,/,**,^,writeln]),
+	length(Params,Params_length),
+	Params_length > 0,
+	get_var(Params,Vars,Params1),
+	append([Name|Params1],[Output1],Input0),
+	Input1 =.. Input0,
+	call(Input1).
